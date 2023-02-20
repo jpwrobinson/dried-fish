8b6589910911765d209dd8848eb974c769e6f1b9
James Robinson <jpwrobinson@gmail.com>
2023-02-20 17:09:14 +0000

    fig 1 dried sp draft

--- scripts/fig/Figure1_nutrient_content.R
+++ scripts/fig/Figure1_nutrient_content.R
@@ -70,42 +70,72 @@ dat<-nutl_agg %>%
                            str_detect(pop, 'Adult women')~rni_women,
                            str_detect(pop, 'Adult men')~rni_men,
                            str_detect(pop, 'Pregnant')~rni_pregnant)) %>% 
-    mutate(rni = rni/portion/100) %>%
+    mutate(rni = rni/portion*100/100) %>% ## correct portion size (portion * 100) then rescale between 0-1
     ## cap nutrient RDA at 100% (i.e. a species either meets (100%) or doesn't meet (<100%) the RDA)
     mutate(rni = case_when(rni > 1 ~ 1, TRUE ~ rni))
 
 sp<-unique(dat$species)
+sp<-sp[!sp == '']
+
+th<-theme(plot.subtitle = element_text(size=9, colour='black', face=3, hjust=0),
+        legend.position = 'none') 
+
 for(i in 1:length(sp)){
     
     plotter<-dat[,c('form', 'nutrient', 'rni', 'species')] %>% 
         filter(species == sp[i]) %>% select(-species) %>% 
-        pivot_wider(names_from = nutrient, values_from = rni) %>% select_if(~ !any(is.na(.)))
+        filter(form != 'Fresh') %>% # drop fresh
+        mutate(rni = ifelse(is.na(rni), 0, rni)) %>% 
+        pivot_wider(names_from = nutrient, values_from = rni) 
+        # select_if(~ !any(is.na(.)))
+        
     
-    if(i != 1){names(plotter)<-c('form', 'I', 'Vit-A', 'Vit-B12', 'Vit-D', 'Vit-B9', )}
-    gg<-ggradar(plotter, 
+    if(i != 1){
+        ## All panels without top-left guide
+        names(plotter)<-c('form','Ca', 'Fe', 'Se', 'Zn', 'I', 'v-A', 'v-B12','v-D', 'v-B9')
+        gg<-ggradar(plotter, 
             group.colours = pcols,
             base.size = 1,
+            # values.radar = '',
+            grid.label.size = 3,
             group.point.size = 2,
             group.line.width = 1,
             background.circle.colour = "white",
-            axis.label.size = 4,
+            axis.label.size = 3,
             fill=TRUE,
-            gridline.mid.colour = "grey") + 
-        labs(
-            # title = tit,
-            subtitle = sp[i],
-            # caption = cap
-        ) +
-        coord_equal(clip='off') +
-        theme(
-            plot.title = element_text(size=14, colour='black', face=2, hjust=1),
-            plot.subtitle = element_text(size=11, colour='black', face=3, hjust=1),
-            plot.caption = element_text(size=12, colour='#636363', face=3),
-            legend.position = 'none') + 
-        scale_color_manual(values=pcols_named) + ## second col/fill scales to assign specific sample forms
-        scale_fill_manual(values=pcols_named) 
-    
+            gridline.mid.colour = "grey") +
+            th + labs(subtitle = sp[i]) +coord_equal(clip='off') +
+            scale_color_manual(values=pcols_named) + scale_fill_manual(values=pcols_named) 
+            
+    } else {
+        ## Top-left guide
+        names(plotter)[10]<-'Vitamin B9'
+        gg<-ggradar(plotter, 
+                    group.colours = pcols,
+                    base.size = 1,
+                    group.point.size = 2,
+                    grid.label.size  = 3,
+                    group.line.width = 1,
+                    background.circle.colour = "white",
+                    axis.label.size = 3,
+                    fill=TRUE,
+                    gridline.mid.colour = "grey") +
+            th + labs(subtitle = sp[i]) +coord_equal(clip='off') +
+            scale_color_manual(values=pcols_named) + scale_fill_manual(values=pcols_named) 
+    }
+        
     assign(paste('gg', i, sep = '_'), gg)
 }
-pl<-list(gg_1, gg_2, gg_3, gg_4, gg_5, gg_6, gg_7, gg_8, gg_9, gg_10, gg_11, gg_12)
-plot_grid(plotlist=pl)
+
+gg_leg<-ggradar(dat %>% filter(form %in% unique(dat$form)) %>% distinct(form, nutrient) %>% 
+                        mutate(rni = 0) %>% 
+                        pivot_wider(names_from = nutrient, values_from = rni), 
+                    fill=TRUE) + guides(color='none') + scale_fill_manual(values=pcols_named[-1]) 
+
+pl<-list(gg_1, gg_2, gg_3, gg_4, gg_5, gg_6, gg_7, gg_8, gg_9, gg_10, gg_11, get_legend(gg_leg))
+
+pdf(file = 'figures/Figure1_dried_content.pdf', height =7, width=12)
+print(
+    plot_grid(plotlist=pl)
+)
+dev.off()
