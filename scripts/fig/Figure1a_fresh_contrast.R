7a09ace71305079d708c25a2725cf647e60b10ae
James Robinson <jpwrobinson@gmail.com>
2023-04-28 15:57:47 +0100

    contrast plots

--- scripts/fig/Figure1a_fresh_contrast.R
+++ scripts/fig/Figure1a_fresh_contrast.R
@@ -13,21 +13,39 @@ nut<-read.csv('data/clean/dried_nutrient_estimates_long.csv') %>%
            nutrient = recode(nutrient,  Epa_dha = 'Omega-3 (DHA + EPA)', 
                              Vitamin_a1 = 'Vitamin A', Vitamin_b12 = 'Vitamin B12', Vitamin_d3 = 'Vitamin D')) 
 
-fresh<-nut %>% filter(form == 'Fresh')
-nut$fresh<-fresh$value[match(nut$id, fresh$id)]
+# save fresh samples, convert fresh values to dry weight equivalents
+fresh<-nut %>% filter(form == 'Fresh') %>% 
+    group_by(id, latin_name, local_name, nutrient) %>% 
+    summarise(value = mean(value), dry_matter_g_100g = mean(dry_matter_g_100g)) %>% 
+    ungroup() %>% rowwise() %>% 
+    mutate(value_dw = value * 100/dry_matter_g_100g) ## convert 100g fresh to 100g dried, based on estimated dry matter content
 
-nut<-nut %>% filter(!is.na(fresh) & form != 'Fresh') %>% 
-    select(sample_id, latin_name, nutrient, unit, fresh, form, value) %>%
-    mutate(contrast = (value - fresh) / fresh * 100)
+nut$fresh_value_dw<-fresh$value_dw[match(nut$id, fresh$id)]
 
-nut_mean<-nut %>% group_by(nutrient) %>% 
+# how many samples do we have fresh and processed?
+nut %>% filter(!is.na(fresh_value_dw)) %>% distinct(latin_name, local_name) # 9 species
+
+nut<-nut %>% filter(!is.na(fresh_value_dw) & form != 'Fresh') %>% 
+    select(sample_id, latin_name, nutrient, unit, fresh_value_dw, form, value) %>%
+    mutate(contrast = (value - fresh_value_dw) / fresh_value_dw)
+
+nut_mean<-nut %>% group_by(nutrient, form) %>% 
     summarise(se = funk::se(contrast), contrast=mean(contrast)) %>% 
     mutate(lwr = contrast - 2*se, upr = contrast + 2*se)
 
-g1A<-ggplot(nut_mean, aes(contrast, fct_reorder(nutrient, contrast))) +
+nut_mean2<-nut %>% group_by(nutrient) %>% 
+    summarise(se = funk::se(contrast), contrast=mean(contrast)) %>% 
+    mutate(lwr = contrast - 2*se, upr = contrast + 2*se)
+
+g1A<-ggplot(nut_mean2, aes(contrast, fct_reorder(nutrient, contrast))) +
+    geom_vline(xintercept = 0, linetype=5, col='grey80')+
     geom_jitter(data = nut, aes( col = form), height=0.1, size=1, alpha=0.5) +
     geom_pointrange(aes(xmin = lwr, xmax = upr)) +
+    # geom_pointrange(aes(xmin = lwr, xmax = upr, fill=form), size=0.5, pch=21, col='black') +
     scale_colour_manual(values=pcols_named[-1]) +
+    scale_fill_manual(values=pcols_named[-1]) +
     labs(x = 'Change in nutrient content after processing', y = '') +
-    scale_x_continuous(limits=c(0, 1100), breaks = seq(0, 2500, 250), labels=paste0(seq(0, 2500, 250), '%')) +
+    scale_x_continuous(limits=c(-1, 2), breaks=seq(-1, 2, by = 0.5), labels=scales::percent) +
     theme(legend.title = element_blank(), legend.position = c(0.8, 0.4))
+g1A
+
--- scripts/summary_stats.R
+++ scripts/summary_stats.R
@@ -14,3 +14,10 @@ dried %>% group_by(local_name, latin_name) %>% summarise(N = n_distinct(sample_i
 # cadmium
 avgs<-dried %>% group_by(nutrient, type, unit) %>% summarise(mu = mean(value))
 avgs %>% filter(nutrient=='cadmium') ## 0.8 mug / 100g for wet weight in Sroy et al. 2023. Corresponds to 2.49 mug in this dataset.
+
+
+## epa + dha
+dried %>% filter(nutrient %in% c('epa', 'dha')) %>% group_by(form, nutrient, unit) %>% 
+    reframe(n_samples = n_distinct(sample_id), min=min(value),max=max(value), mean=mean(value)) %>% 
+    filter(form %in% c('Powder', 'Smoked', 'Sun-dried')) %>% 
+    select(form, nutrient, min:mean, unit, n_samples)
