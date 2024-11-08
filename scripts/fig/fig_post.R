d847c4adf2cb1e77ca3058153ad0bf4efe85062f
James Robinson <jpwrobinson@gmail.com>
2024-11-08 17:03:49 +0000

    int plot

--- scripts/fig/fig_post.R
+++ scripts/fig/fig_post.R
@@ -41,21 +41,51 @@ fig_post<-function(dat){
     
     
     # country level intercepts
-    posterior <- mcmc_intervals_data(m2, transformations = inv_logit)
-    posterior2 <- mcmc_intervals_data(m3, transformations = inv_logit)
+    # posterior <- mcmc_intervals_data(m2, transformations = inv_logit)
+    # posterior2 <- mcmc_intervals_data(m3, transformations = inv_logit)
+    # 
+    # posterc<-rbind(posterior %>% mutate(fish = 'Dried'),
+    #                posterior2 %>% mutate(fish = 'Fresh')) %>% 
+    #     filter(str_detect(parameter, 'r_country\\[')) %>% 
+    #     mutate(country = str_replace_all(parameter, 't\\(r_country\\[', ''),
+    #            country = str_replace_all(country, ',Intercept\\]\\)', ''))
     
-    posterc<-rbind(posterior %>% mutate(fish = 'Dried'),
-                   posterior2 %>% mutate(fish = 'Fresh')) %>% 
-        filter(str_detect(parameter, 'r_country\\[')) %>% 
-        mutate(country = str_replace_all(parameter, 't\\(r_country\\[', ''),
-               country = str_replace_all(country, ',Intercept\\]\\)', ''))
+    mod_sim<-dat %>% 
+        data_grid(Sproximity_to_inland_km = 0,Sproximity_to_marine_km = 0,Sproximity_to_city_mins = 0, Swealth = 0,country=unique(mod_dat$country),Sn_hh = 0)
+    
+    post_mdn<-rbind(
+        mod_sim %>% filter(country=='CIV') %>% 
+            add_epred_draws(m2, ndraws = 100, re_formula = NA) %>% 
+            median_qi() %>% 
+            mutate(m = .epred, m_low = .lower, m_upp = .upper, data = 'Grand mean', fish = 'Dried'),
+        mod_sim %>% filter(country=='CIV') %>% 
+            add_epred_draws(m3, ndraws = 100, re_formula = NA) %>% 
+            median_qi() %>% 
+            mutate(m = .epred, m_low = .lower, m_upp = .upper, data = 'Grand mean', fish = 'Fresh')
+    )
+    
+    posterc<-rbind(
+        mod_sim %>%  
+            add_epred_draws(m2, ndraws = 100, re_formula = ~ (1 | country)) %>% 
+            median_qi() %>% 
+            mutate(m = .epred, ll = .lower, hh = .upper, data = 'Model (expected)', fish = 'Dried') %>% 
+            select(country, m, ll, hh, fish, data),
+        mod_sim %>%  
+            add_epred_draws(m3, ndraws = 100, re_formula = ~ (1 | country)) %>% 
+            median_qi() %>% 
+            mutate(m = .epred, ll = .lower, hh = .upper, data = 'Model (expected)', fish = 'Fresh') %>% 
+            select(country, m, ll, hh, fish, data)
+    )
+    
     
     ga<-ggplot(posterc, aes(country, m, col=fish )) +
-        geom_vline(xintercept = 0, col='grey') +
+        # geom_hline(data = post_mdn, aes(yintercept = m, col=fish), linetype=5) +
+        # geom_rect(data = post_mdn, aes(ymax = m_upp, ymin = m_low, xmin = -Inf, xmax=Inf, fill=fish),alpha=0.5) +
         geom_pointrange(data = posterc, aes( ymin = ll, ymax = hh), position = position_dodge(width=0.5)) +
-        geom_pointrange(data=posterc, linewidth=1, aes(ymax=h, ymin =l), position = position_dodge(width=0.5)) +
+        # geom_pointrange(data=posterc, linewidth=1, aes(ymax=h, ymin =l), position = position_dodge(width=0.5)) +
         scale_y_continuous(labels = scales::label_percent()) +
         scale_colour_manual(values = pcols_named) +
+        # scale_fill_manual(values = pcols_named) +
         scale_x_discrete(limits=levels(mod_dat$country)[c(1,4,3,2,6,5)]) +
         labs(x = '', y = ylab) +
         theme(legend.position = 'none',
